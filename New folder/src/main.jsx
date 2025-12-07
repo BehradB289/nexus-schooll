ba7f652f1a97976ad1333e3969b0864748b440f6
BehradB289 <behradb2xbox@gmail.com>
2025-12-08 00:27:13 +0330

    Add files via upload

---
+++ New folder/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NexusApp from './NexusApp.jsx'
+import './index.css' 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NexusApp />
+  </React.StrictMode>,
+)
